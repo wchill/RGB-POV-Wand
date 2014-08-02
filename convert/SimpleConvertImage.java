38e89f4ef8ebff147855d99678ac09b29a68d4af
wchill <wchill1337@gmail.com>
2014-08-01 20:38:06 -0500

    Create SimpleConvertImage.java

---
+++ convert/SimpleConvertImage.java
@@ -0,0 +1,39 @@
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+ 
+public class SimpleConvertImage {
+        public static void main(String[] args) throws IOException{
+                BufferedImage img=ImageIO.read(new File(args[1]));
+                if(img.getWidth() == 0 || img.getWidth() > 127 || img.getHeight() != 8) {
+                        System.err.println("Image size must be between 1 and 127 pixels wide inclusive and 8 pixels tall");
+                } else {
+                        char[] data = new char[img.getWidth() * img.getHeight() + 2];
+                        data[0] = (char) ((img.getWidth() & 0xFF) >> 8);
+                        data[1] = (char) (img.getWidth() & 0xFF);
+                        for(int i = 0; i < img.getWidth(); i++) {
+                                for(int j = 0; j < 8; j++) {
+                                        int px = img.getRGB(i, j);
+                                        int red = px >> 16;
+                                        int green = (px >> 8) & 0xFF;
+                                        int blue = px & 0xFF;
+                                        char r = (char) (red & 0xE0);
+                                        char g = (char) ((green >> 3) & 0x1C);
+                                        char b = (char) ((blue >> 6) & 0x03);
+                                        char c = (char) (r | g | b);
+                                        data[8 * i + j + 2] = c;
+                                }
+                        }
+                        System.out.println("PROGMEM prog_uchar image[] = {" + data[0] + "," + data[1] + ",");
+                        for(int i = 2; i < img.getWidth() - 1; i++) {
+                                System.out.print(data[2]);
+                                System.out.print(",");
+                        }
+                        System.out.print(data[data.length - 1]);
+                        System.out.println("\n};");
+                }
+        }
+}
